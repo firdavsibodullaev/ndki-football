68ae248d31240e1a193c71a6c7e71d500ed8e9f1
Firdavs Ibodullaev <firdavs3253@gmail.com>
2023-06-15 08:06:44 +0500

    initial commit

---
+++ app/Models/Season.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
+
+/**
+ * @property-read int $id
+ * @property string $name
+ * @property Carbon $started_at
+ * @property Carbon $finished_at
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ */
+class Season extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'started_at',
+        'finished_at'
+    ];
+}
